90ee15de1e9892b45a72be5d88c546066d659f29
Jeffrey Wigington <jwigington22@gmail.com>
2023-03-18 00:06:41 -0600

    add scorecard image

--- Intro.py
+++ Intro.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="Rainbow Dice Intro",
@@ -15,6 +16,9 @@ st.markdown(
     in-app cards are suppored here. The object of the game is to get as many of your squares marked on
     on each row as possible (exept for penalties) to score max points. Each line of numbers must be 
     marked from **_left_ to _right_** only. Once a number has been marked on a line, all unmarked numbers
-    to the left are no longer useable.
+    to the left are no longer useable.\n
 """
 )
+scorecard = Image.open('rd_scorecard.png')
+
+st.image(scorecard, caption='Rainbow Dice Scorecard Example')
